62f54ff7c18b6c0674b5b8cebd0c4f1fb7d2be08
111mihir <mihir.khandekar91@gmail.com>
2022-09-21 12:58:17 +0530

    Revert "moved credentials to env variables"
    This reverts commit 0c5367e76cbe5ea6ff2d8e522bc89473be077e40.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,9 @@
 export const ETHSCAN_API_ENDPOINT = "https://api.etherscan.io/api";
+export const ETHSCAN_API_KEY = "PUUP7XDKX5DSGC5CAMVQH78CC9QA3HXU6E";
 export const ETHSCAN_GET_TRANSACTIONS_API_ENDPOINT =
   "?module=account&action=txlist&startblock=0&endblock=99999999&sort=asc&offset=20&apikey=" +
-  process.env.ETHSCAN_API_KEY;
+  ETHSCAN_API_KEY;
 export const HASURA_POSTGRES_DB_ENDPOINT =
   "https://stable-sunbird-40.hasura.app/v1/graphql";
+export const HASURA_ADMIN_SECRET =
+  "p9dVDeeqzeJZvcEdBAvkJq9JoVl43s9P6xEjrCqBHVLzkq5Xa4BB19xTx1bTdxBn";
